3fcc994d43f6ab2ff53530b110f6e94b57b8d420
makamekm <makame@mail.ru>
2018-03-29 13:51:57 +0000

    Added Redis.ts

--- src/Modules/Redis.ts
+++ src/Modules/Redis.ts
@@ -1,29 +1,30 @@
-import * as redis "redis";
-const { promisify } = require('util');
+import * as redis from "redis";
+import { promisify } from 'util';
 
 import { getConfig } from '../Modules/Config';
 
 export class RedisClient {
-  getAsync: (key: string) => Promise()
-  setAsync: (key: string, value: any) => Promise()
+    
+    getAsync: (key: string) => Promise<void>
+    setAsync: (key: string, value: any) => Promise<void>
 
-  constructor(client) {
-    const getAsync = promisify(client.get).bind(client);
-    const setAsync = promisify(client.set).bind(client);
-  }
+    constructor(client) {
+        this.getAsync = promisify(client.get).bind(client);
+        this.setAsync = promisify(client.set).bind(client);
+    }
 }
 
-scopes: { [name: string]: RedisClient } = {}
+export const scopes: { [name: string]: RedisClient } = {};
 
-export async function getClient(scope: string = 'Main', init?: boolean): RedisClient {
-  if (init || !this.scopes[scope]) {
-    let client = redis.createClient({
-      port: getConfig().redis[scope].port,
-      host: getConfig().redis[scope].host,
-      password: getConfig().redis[scope].password
-    });
-    this.scopes[scope] = new RedisClient(client);
-  }
-
-  return this.scopes[scope];
+export async function getClient(scope: string = 'Main', init?: boolean): Promise<RedisClient> {
+    if (init || !this.scopes[scope]) {
+        let client = redis.createClient({
+            port: getConfig().redis[scope].port,
+            host: getConfig().redis[scope].host,
+            password: getConfig().redis[scope].password
+        });
+        this.scopes[scope] = new RedisClient(client);
+    }
+    
+    return this.scopes[scope];
 }
