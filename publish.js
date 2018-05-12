647208099e680ad1ca7fb507341a00cfb1f56101
Максим Карпов <makame@mail.ru>
2018-05-12 19:43:41 +0200

    Test

--- publish.js
+++ publish.js
@@ -1,27 +1,27 @@
-const fs = require('fs');
-const path = require('path');
-const { execSync } = require('child_process');
-var client = new (require('npm-registry-client'))({});
+// const fs = require('fs');
+// const path = require('path');
+// const { execSync } = require('child_process');
+// var client = new (require('npm-registry-client'))({});
 
-// Reading the current package.json
-let data = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'package.json')));
+// // Reading the current package.json
+// let data = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'package.json')));
 
-// Changing public data
-data.name = "reiso";
-data.private = false;
-delete data.scripts;
+// // Changing public data
+// data.name = "reiso";
+// data.private = false;
+// delete data.scripts;
 
-// Writing the data
-let strData = JSON.stringify(data, null, 2);
-fs.writeFileSync(path.resolve(__dirname, 'build', 'package.json'), strData);
+// // Writing the data
+// let strData = JSON.stringify(data, null, 2);
+// fs.writeFileSync(path.resolve(__dirname, 'build', 'package.json'), strData);
 
-let strComposer = fs.readFileSync(path.resolve(__dirname, 'composer.js'));
-fs.writeFileSync(path.resolve(__dirname, 'build', 'composer.js'), strComposer);
+// let strComposer = fs.readFileSync(path.resolve(__dirname, 'composer.js'));
+// fs.writeFileSync(path.resolve(__dirname, 'build', 'composer.js'), strComposer);
 
-let strReadme = fs.readFileSync(path.resolve(__dirname, 'README.md'));
-fs.writeFileSync(path.resolve(__dirname, 'build', 'README.md'), strReadme);
+// let strReadme = fs.readFileSync(path.resolve(__dirname, 'README.md'));
+// fs.writeFileSync(path.resolve(__dirname, 'build', 'README.md'), strReadme);
 
-let args = process.argv.slice(2)
+// let args = process.argv.slice(2)
 
 let username = args[0];
 let password = args[1];
@@ -30,17 +30,17 @@ let uri = "registry.npmjs.org";
 
 console.log('Using:', username, email);
 
-async function publish() {
-  await new Promise((resolve, reject) => client.adduser("http://" + uri, { auth: { username, password, email } }, function (err, data, raw, res) {
-    if (err) {
-      reject(err);
-      return;
-    }
-    fs.writeFileSync(path.resolve('./build/.npmrc'), `//<npm-registry>:8080/:_authToken=\n//${uri}\/:_authToken=${data.token}`);
-    resolve();
-  }));
+// async function publish() {
+//   await new Promise((resolve, reject) => client.adduser("http://" + uri, { auth: { username, password, email } }, function (err, data, raw, res) {
+//     if (err) {
+//       reject(err);
+//       return;
+//     }
+//     fs.writeFileSync(path.resolve('./build/.npmrc'), `//<npm-registry>:8080/:_authToken=\n//${uri}\/:_authToken=${data.token}`);
+//     resolve();
+//   }));
 
-  execSync("npm publish", { stdio: [0, 1, 2], cwd: path.resolve(__dirname, 'build') });
-}
+//   execSync("npm publish", { stdio: [0, 1, 2], cwd: path.resolve(__dirname, 'build') });
+// }
 
-publish();
+// publish();
